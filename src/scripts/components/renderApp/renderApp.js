b772db81b98f06945b66c3a1995e145912a1e8a8
Vadim Krinichansky <vadim.krinichansky@outlook.com>
2021-02-05 16:34:02 +0200

    complete task

--- src/scripts/components/renderApp/renderApp.js
+++ src/scripts/components/renderApp/renderApp.js
@@ -1,5 +1,5 @@
 import renderBar from "../renderBar";
-import renderCalendar from "../renderCalendar";
+import renderTeams from "../renderTeams";
 
 const renderApp = () => {
   const appElement = document.getElementById("appRoot");
@@ -7,20 +7,12 @@ const renderApp = () => {
   let activeDate = new Date(currentDate);
 
   renderBar({ appElement, activeDate });
-  renderCalendar({ appElement, activeDate });
+  renderTeams({ appElement, activeDate });
 
   let nextMonthButton = document.querySelector(".calendar-bar__control-button--next");
   let prevMonthButton = document.querySelector(".calendar-bar__control-button--prev");
 
   nextMonthButton.addEventListener("click", () => {
-    // activeDate = new Date(activeDate.getFullYear(), activeDate.getMonth() + 1);
-    // let calendarHeader = document.querySelector(".calendar-bar__header");
-    // calendarHeader.innerHTML = "";
-    // let [monthName, yearValue] = activeDate.toLocaleDateString("en-US", { month: "long", year: "numeric" }).split(" ");
-    // calendarHeader.innerHTML = monthName + " " + yearValue;
-    // let calendarTable = document.querySelector(".calendar-table");
-    // appElement.removeChild(calendarTable);
-    // renderCalendar({ appElement, activeDate });
     activeDate = buttonAction("next", activeDate, appElement);
   });
 
@@ -47,7 +39,8 @@ const renderApp = () => {
     calendarHeader.innerHTML = monthName + " " + yearValue;
     let calendarTable = document.querySelector(".calendar-table");
     appElement.removeChild(calendarTable);
-    renderCalendar({ appElement, activeDate });
+    renderTeams({ appElement, activeDate });
+
     return activeDate;
   }
 };
